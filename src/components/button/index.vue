7d673d1bf9bfc8ede58f8e3fc19b3c35f50d62f7
dawpf <1797526060@qq.com>
2020-05-06 19:27:44 +0800

    项目配置，保存时关闭eslint校验

---
+++ src/components/button/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container_button">
+    <h1>这是button组件</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Button></Button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Button from '@c/button/index.vue'
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+  name: 'Home',
+  components: { Button }
+}
 </script>
